a0b4baf04dda9c8c0651203a76df5efaf665d494
mousedoc <sangyun3631@gmail.com>
2023-11-09 18:45:28 +0900

    get all selected clips check

--- main.jsx
+++ main.jsx
@@ -7,3 +7,40 @@ $.write(numVideoTrackLog);
 
 numAudioTrackLog = "Number of Audio Tracks: " + audioTracks.numTracks;
 $.write(numAudioTrackLog);
+
+var a = GetAllSelectedTrackItems();
+$.write(a.length);
+
+function GetAllSelectedTrackItems() {
+    items = [];
+
+    if (videoTracks != "") {
+        for (var track in videoTracks) {
+            items.push(GetAllSelectedTrackItems(track));
+        }
+    }
+
+    if (audioTracks != "") {
+        for (var track in audioTracks) {
+            items.push(GetAllSelectedTrackItems(track));
+        }
+    }
+
+    return items;
+}
+
+function GetAllSelectedTrackItems(track) {
+    items = [];
+
+    if (track == "")
+        return items;
+
+    var clips = track.clips;
+    for (var clip in clips) {
+        if (clip.isSelected()) {
+            items.push(clip);
+        }
+    }
+
+    return items;
+}
